47e2a6b4ef562d47916ed918a8666e035e158d2a
Daniel Sanchez <daniel.sanchez@apriorins.com>
2024-08-06 18:45:52 -0500

    Se crea el proyecto con Netbeans.

---
+++ CalcularModulo/src/main/java/com/mycompany/calcularmodulo/CalcularModulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcularmodulo;
+
+/**
+ *
+ * @author danielsanchez
+ */
+public class CalcularModulo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

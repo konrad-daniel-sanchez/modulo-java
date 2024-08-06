0d1de7eec278cf5d69df28d3fdea6143df724dc5
Daniel Sanchez <daniel.sanchez@apriorins.com>
2024-08-06 18:55:49 -0500

    Se calcula el módulo.

--- CalcularModulo/src/main/java/com/mycompany/calcularmodulo/CalcularModulo.java
+++ CalcularModulo/src/main/java/com/mycompany/calcularmodulo/CalcularModulo.java
@@ -3,14 +3,23 @@
  */
 
 package com.mycompany.calcularmodulo;
+import java.util.*;
 
 /**
  *
  * @author danielsanchez
  */
 public class CalcularModulo {
+    
+    public static int calcularModulo(int n1, int n2){
+        return n1 % n2;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner lector = new Scanner(System.in);
+        int numero1 = lector.nextInt();
+        int numero2 = lector.nextInt();
+        int resultado = calcularModulo(numero1, numero2);
+        System.out.println("El resultado del módulo es " + resultado);
     }
 }
